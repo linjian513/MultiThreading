d1b61b836a9321c55936ca0bbe11c405f3fd6213
linjian513@163.com <linjian513@163.com>
2017-10-31 22:42:39 +0800

    提交

---
+++ src/main/java/org/linjian/MultiThreading/App.java
@@ -0,0 +1,13 @@
+package org.linjian.MultiThreading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
